ecb87a0658207b925a645554f08f0343f2360b68
ica ngabito <120780323+mdpn22@users.noreply.github.com>
2022-12-19 15:26:38 +0800

    Menambahkan script baru
    Disini saya menambahkan script segitiga arah ke atas

--- uas.php
+++ uas.php
@@ -10,3 +10,18 @@
 	echo "<br>";
 	}
 ?>
+
+//segitiga arah keatas
+
+<?php
+	$star=10;
+	for( $a=$star;$a>0;$a--){
+	for($i=1; $i<=$a; $i++){
+		echo "&nbsp";
+	}
+	for($a1=$star;$a1>=$a;$a1--){
+		echo"*";
+	}
+	echo"<br>";
+	}
+?>
