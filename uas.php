c559b13b70663e70a6bb6cc0ee530a72111b1689
Aulia nh <120780075+aulianurfh@users.noreply.github.com>
2022-12-19 15:02:22 +0700

    Menambah script
    Menambah script segitiga belah ketupat pada uas.php

--- uas.php
+++ uas.php
@@ -10,3 +10,28 @@
 	echo "<br>";
 	}
 ?>
+
+//segitiga belah ketupat
+
+<?php
+  $star=10;
+  for($a=1;$a<=$star;$a++){
+  for($b=$star;$b>=$a;$b=1){
+	  echo "&nbsp";
+  }
+  for($c=1;$c<=$a; $a++){
+	  echo "*";
+  }
+	echo "<br>";
+  }
+  for($a=1; $a<=$star; $a++){
+  for($b=1; $b<=$a; $b++){
+	echo "&nbsp";
+  }
+  for($c=$star, $c>=$a;$c=1){
+	  echo "*";
+  }
+  echo "<br>";
+  }
+?>
+      
